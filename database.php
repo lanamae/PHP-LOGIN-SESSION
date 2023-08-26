bc3288f86a7db34a6d0c7107d550832326664a5f
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2023-08-26 16:29:54 +0800

    Recreate files

--- database.php
+++ database.php
@@ -1,17 +1,7 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'php-login-session';
 
-$connection = mysqli_connect($host, $user, $password, $database);
 
-if(mysqli_connect_error()){
-    echo "error";
 
-}
 
-// else{
-//     echo 'connected';
-// }
+?>
